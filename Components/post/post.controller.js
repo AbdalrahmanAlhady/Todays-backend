e355cb5a5c7420441420e0c0b04bd305a273ccc4
Abdelrahman Alhady <abdelrahman.alhady@widebot.net>
2023-08-05 23:31:55 +0300

    finalize post component

--- Components/post/post.controller.js
+++ Components/post/post.controller.js
@@ -12,6 +12,19 @@ export const createPost = async (req, res) => {
   }
 };
 
+export const getPostById = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post) {
+      res.status(201).json({ post });
+    } else {
+      res.status(404).json({ message: "not found" });
+    }
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
+
 export const getPosts = async (req, res) => {
   try {
     const features = new APIFeatures(Post.find(), req.query)
@@ -20,7 +33,31 @@ export const getPosts = async (req, res) => {
       .limitFields()
       .paginate();
     const posts = await features.query;
-    res.status(200).json({posts})
+    res.status(200).json({ posts });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  try {
+    const body = req.body.body;
+    const post = await Post.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: { body } },
+      { new: true }
+    );
+    res.status(200).json({ post });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+    console.log(error.message);
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.deleteOne({ _id: req.params.id });
+    res.status(200).json({ post });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
--- Components/post/post.router.js
+++ Components/post/post.router.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { createPost,getPosts } from "./post.controller.js";
+import { createPost,deletePost,getPostById,getPosts, updatePost } from "./post.controller.js";
 
 const router = new Router();
 
 router.post("/", createPost);
 
 router.get("/",getPosts)
+router.get("/:id",getPostById)
+
+router.put("/:id",updatePost)
+
+router.delete("/:id",deletePost)
 
 
 export default router;
