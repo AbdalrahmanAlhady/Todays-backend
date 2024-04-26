305fec6dd8951a10246a68686c31436b3b43d5fb
AbdalrahmanAlhady <abdalrahmanmalhady.2000@gmail.com>
2024-04-27 02:17:40 +0300

    make db migrationable

---
+++ DBs/config.js
@@ -0,0 +1,15 @@
+export default {
+  development: {
+    username: "root",
+    password: "123456",
+    database: "today's_db",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    // ... other configurations
+  },
+  production: {
+    // ... other configurations
+  },
+};
